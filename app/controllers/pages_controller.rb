31300fd649bb01bc8605d30edf6a44c8297ef688
cosmina iosifescu <cosmina.iosifescu@gmail.com>
2021-12-30 10:55:17 +0100

    basic setup of pages controller, routes and view

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def today
+  end
+
+  def trends
+  end
+
+  def goals
+  end
+
+  def settings
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'today', to: 'pages#today', as: :today
+  get 'trends', to: 'pages#trends', as: :trends
+  get 'goals', to: 'pages#goals', as: :goals
+  get 'settings', to: 'pages#settings', as: :settings
 end
